ff0ccf3c48e9daa7cb036e86bd6b5ae083472753
Shadow01xx <bryanfelipedasilvasantos@gmail.com>
2025-08-17 01:49:18 -0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import DiscordProvider from "next-auth/providers/discord"
 const handler = NextAuth({
   providers: [
     DiscordProvider({
-      clientId: process.env.1405318215948832858,
-      clientSecret: process.env.I5ftA6Su8DU5iM60YSoHl-RYbMc-mCRd,
+      clientId: process.env.1405318215948832858!,
+      clientSecret: process.env.I5ftA6Su8DU5iM60YSoHl-RYbMc-mCRd!,
     }),
   ],
   callbacks: {
